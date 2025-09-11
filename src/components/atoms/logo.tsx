eec8ab44d7c12d47a3cfb076c2573eca61810a73
Hervé Zossou <hervezossou8@gmail.com>
2025-09-11 06:27:25 +0100

    feat(atoms): added logo component

---
+++ src/components/atoms/logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+    return (
+        <div className="flex items-center justify-center size-max">
+            <h1 className="font-bold font-logo text-3xl gradint">LOGO</h1>
+        </div>
+    )
+}
