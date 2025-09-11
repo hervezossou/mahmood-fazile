b9e47b8d02790fbc4530b4d72576560d8fd0ba72
Hervé Zossou <hervezossou8@gmail.com>
2025-09-11 10:04:10 +0100

    feat(data): added nav items array

---
+++ src/lib/data.ts
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    label: "Home",
+    href: "#home",
+  },
+  {
+    label: "Services",
+    href: "#services",
+  },
+  {
+    label: "About me",
+    href: "#about-me",
+  },
+  {
+    label: "Portfolio",
+    href: "#portfolio",
+  },
+  {
+    label: "Contact me",
+    href: "#contact-me",
+  },
+]
