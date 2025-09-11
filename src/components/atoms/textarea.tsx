73c7e28cf92ef75237239547bc343443a29f7f1b
Hervé Zossou <hervezossou8@gmail.com>
2025-09-11 20:51:25 +0100

    feat(atoms): created textarea component

---
+++ src/components/atoms/textarea.tsx
@@ -0,0 +1,38 @@
+import type { ChangeEvent } from "react";
+
+interface TextareaProps {
+  label: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  name: string;
+  placeholder?: string;
+}
+
+export const Textarea = ({
+  label,
+  value,
+  onChange,
+  name,
+  placeholder,
+}: TextareaProps) => {
+  return (
+    <div className="w-full flex flex-col gap-1">
+      <label
+        htmlFor={name}
+        className="text-sm font-medium text-fondation-white-normal sr-only"
+      >
+        {label}
+      </label>
+      <textarea
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full lg:w-[491px] bg-white/5 rounded-lg px-6 py-3.5 text-fondation-white-dark-hover placeholder:text-fondation-white-dark-hover focus:outline-2 focus:outline-fondation-orange-normal"
+        rows={5}
+        aria-required="true"
+      />
+    </div>
+  );
+};
