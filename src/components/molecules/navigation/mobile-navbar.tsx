e69cbbac416f95dd86ea23189e3a2679d56f208c
Hervé Zossou <hervezossou8@gmail.com>
2025-09-11 10:12:58 +0100

    feat(mobile-navbar): added mobile navbar component

---
+++ src/components/molecules/navigation/mobile-navbar.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from "@/components/atoms/nav-link"
+import { navItems } from "@/lib/data"
+
+export const MobileNavBar = () => {
+  return (
+    <nav className="flex items-center justify-between max-w-[653px] h-6 gap-15">
+      {navItems.map((item, index) => (
+        <NavLink href={item.href} label={item.label} key={index} />
+      ))}
+    </nav>
+  )
+}
