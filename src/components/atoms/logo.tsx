1ff30132d0f00840ceccd94f3a3e1f5a5eb10673
Hervé Zossou <hervezossou8@gmail.com>
2025-09-11 06:30:09 +0100

    feat(atoms): updated gradient to linear-gradient

--- src/components/atoms/logo.tsx
+++ src/components/atoms/logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div className="flex items-center justify-center size-max">
-            <h1 className="font-bold font-logo text-3xl gradint">LOGO</h1>
+            <h1 className="font-bold font-logo text-2xl linear-gradient lg:text-3xl">LOGO</h1>
         </div>
     )
 }
