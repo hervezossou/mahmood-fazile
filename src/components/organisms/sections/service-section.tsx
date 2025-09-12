39cbf86b026cbc994260e2e038926a2013f268c1
Hervé Zossou <hervezossou8@gmail.com>
2025-09-12 18:15:32 +0100

    feat(sections): create services section

---
+++ src/components/organisms/sections/service-section.tsx
@@ -0,0 +1,27 @@
+import { SectionHeader } from "@/components/molecules/section-header"
+import ServiceCard from "@/components/molecules/service-card"
+import { services } from "@/lib/data"
+
+export const ServicesSection = () => {
+    return (
+        <section
+            id="services"
+            className="w-full flex flex-col items-center gap-10 lg:gap-12.5"
+        >
+            <SectionHeader
+                title="Services"
+                subtitle="Lorem ipsum dolor sit amet consectetur. Imperdiet convallis blandit felis ligula aliquam"
+            />
+            <div className="w-full grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-10">
+                {services.map((service) => (
+                    <ServiceCard
+                        key={service.id}
+                        name={service.name}
+                        icon={service.icon}
+                        description={service.description}
+                    />
+                ))}
+            </div>
+        </section>
+    )
+}
