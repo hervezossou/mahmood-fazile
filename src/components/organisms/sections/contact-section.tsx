c3c435094f77e2be5dc3f35e99cad104a91c7cc8
Hervé Zossou <hervezossou8@gmail.com>
2025-09-12 15:54:30 +0100

    feat(contact-section): added id

--- src/components/organisms/sections/contact-section.tsx
+++ src/components/organisms/sections/contact-section.tsx
@@ -3,7 +3,10 @@ import { ContactForm } from "@/components/organisms/contact-form";
 
 export const ContactSection = () => {
   return (
-    <section className="w-full flex flex-col items-center gap-10 lg:gap-12.5">
+    <section
+      id="contact-me"
+      className="w-full flex flex-col items-center gap-10 lg:gap-12.5"
+    >
       <SectionHeader
         title="Contact me"
         subtitle="Cultivating Connections: Reach Out and Connect with Me"
