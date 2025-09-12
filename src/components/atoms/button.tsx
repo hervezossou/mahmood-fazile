86578afe8354b8bd55bc43021f0badaaba859f53
Hervé Zossou <hervezossou8@gmail.com>
2025-09-12 18:47:30 +0100

    feat(button): added icon props

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,15 +1,17 @@
+import { Icon } from "@iconify/react"
 import type { ButtonVariant } from "@/lib/types";
 import clsx from "clsx";
 
 interface ButtonProps {
   label: string;
+  icon?: string;
   variant: ButtonVariant;
   onClick?: () => void;
 }
 
-export const Button = ({ label, variant, onClick }: ButtonProps) => {
+export const Button = ({ label, icon, variant, onClick }: ButtonProps) => {
   const baseStyle =
-    "w-max h-[33px] lg:h-[43px] flex items-center justify-center px-5 py-2 rounded-md lg:px-10 lg:py-3 gap-2.5 font-medium lg:font-bold font-display text-sm lg:text-base cursor-pointer";
+    "w-max h-[33px] lg:h-[48px] flex items-center justify-center px-5 py-2 rounded-md lg:px-8 lg:py-3 gap-2.5 font-medium lg:font-bold font-display text-sm lg:text-base cursor-pointer";
 
   const variantStyle = {
     contained: "bg-gradint text-white",
@@ -23,7 +25,7 @@ export const Button = ({ label, variant, onClick }: ButtonProps) => {
       className={clsx(baseStyle, variantStyle[variant])}
       onClick={onClick}
     >
-      {label}
+      {icon && <Icon icon={icon} width={24} height={24} />}{label}
     </button>
   );
 };
