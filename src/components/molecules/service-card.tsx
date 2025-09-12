472c12371cd01a583f25f1cf4f580980a99577f1
Hervé Zossou <hervezossou8@gmail.com>
2025-09-12 18:13:06 +0100

    feat(service-card): create a service card component

---
+++ src/components/molecules/service-card.tsx
@@ -0,0 +1,37 @@
+import { Icon } from '@iconify/react';
+import clsx from 'clsx';
+
+interface ServiceCardProps {
+    icon: string;
+    name: string;
+    description: string;
+    className?: string;
+}
+
+const ServiceCard = ({ icon, name, description, className }: ServiceCardProps) => {
+    return (
+        <div
+            className={clsx(
+                'flex flex-col items-center justify-center w-full bg-white/4 transition-colors ease-in-out duration-300 md:h-[322px] p-[34px] gap-4 group hover:bg-fondation-orange-normal md:gap-[34px] rounded-2xl md:rounded-3xl',
+                className
+            )}
+        >
+            <div className="flex flex-col items-center gap-2">
+                <Icon
+                    icon={icon}
+                    width={34}
+                    height={34}
+                    className="text-fondation-orange-normal group-hover:text-fondation-white-normal"
+                />
+                <h3 className="text-xl text-center font-bold text-fondation-orange-normal group-hover:text-fondation-white-normal leading-[160%]">
+                    {name}
+                </h3>
+            </div>
+            <p className="font-medium text-base text-center text-fondation-white-darker group-hover:text-fondation-white-normal md:text-xl">
+                {description}
+            </p>
+        </div>
+    );
+};
+
+export default ServiceCard;
