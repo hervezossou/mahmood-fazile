1ad648476c963c98b6ed2d117fef83ffd2d55f0b
Hervé Zossou <hervezossou8@gmail.com>
2025-09-15 17:46:51 +0100

    feat(mobile-navbar): refactor the strcuture or mobile nav panel

--- src/components/molecules/navigation/mobile-navbar.tsx
+++ src/components/molecules/navigation/mobile-navbar.tsx
@@ -1,11 +1,19 @@
 import { NavLink } from "@/components/atoms/nav-link";
 import { navItems } from "@/lib/data";
 
-export const MobileNavBar = () => {
+interface MobileNavPanelProps {
+  isOpen: boolean;
+}
+
+export const MobileNavPanel = ({ isOpen }: MobileNavPanelProps) => {
   return (
-    <nav className="flex items-center justify-between max-w-[653px] h-6 gap-15">
+    <nav className={`absolute top-17 left-0 z-50 w-full h-screen flex flex-col bg-fondation-grey-dark items-center gap-10 p-10 transform transition-transform ease-in-out duration-500 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
       {navItems.map((item, index) => (
-        <NavLink href={item.href} label={item.label} key={index} />
+        <NavLink
+          href={item.href}
+          label={item.label}
+          key={index}
+        />
       ))}
     </nav>
   );
