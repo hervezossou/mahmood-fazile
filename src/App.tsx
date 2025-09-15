1ab81d3cea3f52f74081f84ba31bde75963ff4e8
Hervé Zossou <hervezossou8@gmail.com>
2025-09-15 17:51:49 +0100

    feat(app): added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 
+import { Header } from "@/components/layout/header";
+import { HeroSection } from "@/components/organisms/sections/hero-section";
+import { ServicesSection } from "@/components/organisms/sections/service-section";
 import { AboutSection } from "@/components/organisms/sections/about-section";
 import { ContactSection } from "@/components/organisms/sections/contact-section";
 import { Footer } from "@/components/layout/footer";
@@ -16,7 +19,10 @@ export default function App() {
 
   return (
     <>
+      <Header />
       <main className="w-full flex flex-col gap-20 px-5 md:px-10 lg:px-20 lg:gap-37.5">
+        <HeroSection />
+        <ServicesSection />
         <AboutSection />
         <ContactSection />
       </main>
