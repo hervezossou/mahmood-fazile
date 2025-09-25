1bc3282dc0b88916248aaf9404034caa2dc9a6e5
Hervé Zossou <hervezossou8@gmail.com>
2025-09-25 10:11:05 +0100

    refactor(exprtise-card): refactor expertise card props

--- src/components/molecules/expertise-card.tsx
+++ src/components/molecules/expertise-card.tsx
@@ -1,19 +1,19 @@
 import clsx from "clsx";
+import type { Expertise } from "@/lib/types";
 
 interface ExpertiseCardProps {
-    value: string
-    label: string;
-    hasBorder: boolean
+    expertise: Expertise;
+    hasBorder: boolean;
 }
 
-export const ExpertiseCard = ({ value, label, hasBorder }: ExpertiseCardProps) => {
+export const ExpertiseCard = ({ expertise, hasBorder }: ExpertiseCardProps) => {
     return (
         <div className={clsx("flex flex-col items-start gap-3 py-1.5", `${hasBorder && "pr-4 lg:pr-7.5 border-fondation-white-dark-hover border-r-1"}`)}>
             <h4 className="font-extrabold text-xl text-[#FD6F00] xl:text-2xl">
-                {value}
+                {expertise.value}
             </h4>
             <span className="font-bold text-base text-nowrap text-fondation-white-normal-hover xl:text-xl">
-                {label}
+                {expertise.label}
             </span>
         </div>
     )
